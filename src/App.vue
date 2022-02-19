c7fc06ce1610c76634e8d539a67fb9a17b38fdd1
sk3ndi- <thedev.kenalbert@gmail.com>
2022-02-19 23:00:14 +0800

    Updated

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script>
 import Navigation from './components/Navigation.vue'
-//import Home from './views/Home.vue'
+import Home from './views/Home.vue'
 import Footer  from './components/Footer.vue'
 import MobileMenu  from './components/MobileMenu.vue'
 
