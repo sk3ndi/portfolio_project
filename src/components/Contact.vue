23300308ea8f4530e3afa253de81134c631bb0a5
sk3ndi- <thedev.kenalbert@gmail.com>
2022-02-21 01:14:36 +0800

    Updated Files

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,21 +4,21 @@
         <div class="uk-flex-middle uk-grid uk-grid-large uk-height-viewport" uk-grid> <!-- ::::  acts as row  ::: !-->
 
             <!-- Left Side !-->
-            <div class="uk-width-1-2@m uk-animation-scale-up uk-animation" style="animation-delay: 750ms;" uk-grid>
+            <div class="uk-width-1-2@m uk-animation-scale-up uk-animation uk-visible@m" style="animation-delay: 750ms;" uk-grid>
               <div class="uk-width-3-4 uk-padding uk-align-center">
                 <img class="uk-preserve" src="@/assets/svg/email_icon.svg" alt="Email Me Now!"  uk-svg />
               </div>
             </div>
 
             <!-- Right Side !-->
-            <div class="uk-width-expand@m uk-animation-scale-up uk-animation" style="animation-delay: 1050ms;">
+            <div class="uk-width-expand@m uk-animation-scale-up uk-animation uk-text-center uk-text-left@m" style="animation-delay: 1050ms;">
                 <h1 class="title">Have Any Queries?</h1>
                 <p>I'm looking forward to hear from you! I'm open for new feedback, suggestions and oppotunities for my improvement.</p>
                 <p>If you have any questions or need my assistance, feel free to drop me a message on the following icon links below.</p>
                 <p>I'll get back to you as soon as possible.</p>
 
                 <!-- Icon Lists !-->
-                <ul class="uk-iconnav uk-margin-large-top">
+                <ul class="uk-iconnav uk-margin-large-top uk-flex-center uk-flex-left@m">
                   <li><a href="mailto:thedev.kenalbert@gmail.com" class="uk-icon-link uk-margin-small-right" target="_blank" uk-tooltip="title: EMAIL; pos: top-left; delay: 150" uk-icon="icon: mail; ratio: 1.5;"></a></li>
                   <li><a href="skype:tornoalbert?chat" class="uk-icon-link uk-margin-small-right" target="_blank" uk-tooltip="title: SKYPE; pos: top-left; delay: 150" uk-icon="icon: receiver; ratio: 1.5;"></a></li>
                   <li><a href="https://www.linkedin.com/in/albert-t-98155165/" class="uk-icon-link uk-margin-small-right" target="_blank" uk-tooltip="title: LINKEDIN; pos: top-left; delay: 150" uk-icon="icon: linkedin; ratio: 1.5;"></a></li>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,21 +4,21 @@
         <div class="uk-flex-middle uk-grid uk-grid-large uk-height-viewport" uk-grid> <!-- ::::  acts as row  ::: !-->
 
             <!-- Left Side !-->
-            <div class="uk-width-1-2@m uk-animation-scale-up uk-animation" style="animation-delay: 750ms;">
+            <div class="uk-width-1-2@m uk-animation-scale-up uk-animation uk-visible@m" style="animation-delay: 750ms;">
               <div class="uk-width-3-4 uk-padding uk-align-center">
                 <img class="email_img uk-preserve" src="@/assets/svg/dev_img.svg" alt="It's Me Albert!"  uk-svg />
               </div>
             </div>
 
             <!-- Right Side !-->
-            <div class="uk-width-expand@m uk-animation-scale-up uk-animation uk-text " style="animation-delay: 1050ms;">
+            <div class="uk-width-expand@m uk-animation-scale-up uk-animation uk-text uk-text-center uk-text-left@m" style="animation-delay: 1050ms;">
               <h1 class="title">ALBERT</h1>
                 <p class="subheading">Website Editor - Wordpress | Divi | Elementor</p>
                 <p>A simple website I've created just to upload some of my previous works. Feel free to check it out.</p>
                 <p>If you have any queries feel free to drop me a <a href="#contact" uk-scroll>message</a>. Thanks!</p>
 
                 <!-- Icon List !-->
-                <ul class="uk-iconnav uk-margin-large-top">
+                <ul class="uk-iconnav uk-margin-large-top uk-flex-center uk-flex-left@m">
                   <li><a uk-tooltip="title: HTML; pos: top-left; delay: 150" class="fab fa-html5 uk-icon-link uk-margin-small-right"></a></li>
                   <li><a uk-tooltip="title: CSS; pos: top-left; delay: 150" class="fab fa-css3 uk-icon-link uk-margin-small-right"></a></li>
                   <li><a uk-tooltip="title: WORDPRESS; pos: top-left; delay: 150" class="fab fa-wordpress uk-icon-link uk-margin-small-right"></a></li>
